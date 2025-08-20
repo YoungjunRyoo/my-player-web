0a40e8424eb65086fa5ee21b503d8ee2a69ee9e9
YoungjunRyoo <yjblack99@gmail.com>
2025-08-19 22:20:44 -0500

    remove all the team logo image and instead fetch the images from cdn
    remove Logo.jsx, Logo.css

--- src/components/ScoreBox.jsx
+++ src/components/ScoreBox.jsx
@@ -1,17 +1,31 @@
-import Logo from './Logo';
+// import Logo from './Logo';  // no longer needed
 import '../css/ScoreBox.css';
 
 function ScoreBox(prop) {
   return (
     <>
       <div className="result-contatiner">
-        <Logo name={prop.away.team.name} score={prop.away.score} />
+        <div className="logo-score">
+          <img
+            className="logo"
+            src={`https://www.mlbstatic.com/team-logos/${prop.away.team.id}.svg`}
+            alt={prop.away.team.name}
+          />
+          <p className="score">{prop.away.score}</p>
+        </div>
 
         <div className="vs-state">
           <p className="vs">vs</p>
         </div>
 
-        <Logo name={prop.home.team.name} score={prop.home.score} />
+        <div className="logo-score">
+          <img
+            className="logo"
+            src={`https://www.mlbstatic.com/team-logos/${prop.home.team.id}.svg`}
+            alt={prop.home.team.name}
+          />
+          <p className="score">{prop.home.score}</p>
+        </div>
       </div>
     </>
   );

f0b274888ca0f85a1ed9b9737271d96d0f1043d2
YoungjunRyoo <yjblack99@gmail.com>
2025-08-21 18:40:12 -0500

    remove footer in app.jsx for test purpose

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
-      <Footer />
+      
     </Router>
   );
 }
